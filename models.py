e9186a57eac77110affb30b3002f67792bd59705
largelymfs <largelymfs@gmail.com>
2015-01-12 19:59:50 +0800

    Update the multi-sense word embedding mode

--- models.py
+++ models.py
@@ -7,7 +7,6 @@ def list2npvector(vect):
 
 class W2VModel:
 
-
     def __init__(self, filename):
         self.load(filename)
 
@@ -42,11 +41,16 @@ class W2VModel:
         return result
 
 class MSW2VModel(W2VModel):
+# the multi-sense word embedding viewer
 
     def __init__(self, filename):
+       #construction function
+
         W2VModel.__init__(self, filename)
 
     def load(self, filename):
+        #load the MSW2VMODEL
+
         print "Loading MultiSense Word2vec Model : ", filename,'...',
         with open(filename) as f:
             tmp = f.readline().strip().split()
@@ -67,6 +71,56 @@ class MSW2VModel(W2VModel):
                 self.content[word]['global_embedding'] = np.array([float(item) for item in f.readline().strip().split()])
                 self.content[word]['sense_embeddings'] = [ np.array([float(item) for item in f.readline().strip().split()]) for i in range(self.content[word]['sense_number'])]
         print "Completed!"
+
+    def similarity(self, word1, sense1, word2, sense2):
+        #if the sense is 0, we use the global embedding
+        #return format : the similarity of (word1, sense1) and (word2, sense2)
+
+        if word1 not in self.content:
+            return 0.0
+        if sense1 == 0:
+            embedding1 = self.content[word1]['global_embedding']
+        else:
+            if sense1 <= self.content[word1]['sense_number']:
+                embedding1 = self.content[word1]['sense_embeddings'][sense1 - 1]
+            else:
+                return 0.0
+
+        if word2 not in self.content:
+            return 0.0
+        if sense2 == 0:
+            embedding2 = self.content[word2]['global_embedding']
+        else:
+            if sense2 <= self.content[word2]['sense_number']:
+                embedding2 = self.content[word2]['sense_embeddings'][sense2 - 1]
+            else:
+                return 0.0
+
+        return np.dot(embedding1, embedding2)/(LA.norm(embedding1) * LA.norm(embedding2))
+
+    def find_N_nearest(self, word, sense, n):
+        #find the nearest global embeddings of the (word, sense)
+        #if the sense is 0, means the word's global word embedding
+        #return format : a list of n items, each item is (word, similarity)
+
+        if word not in self.content:
+            return None
+        if sense > self.content[word]['sense_number']:
+            return None
+        res = [(w, self.similarity(word, sense, w, 0)) for w in self.content if w !=word]
+        res = sorted(res, cmp=lambda x, y:-cmp(x[1],y[1]))[:n]
+        return res
+
+    def find_N_nearest_whole_word(self, word, n):
+        #find the nearest global embeddings of the word under every sense
+        #return format : a list of s's lists, each sublist is the n nearest item (word, similarity)
+
+        if word not in self.content:
+            return None
+        res = [ self.find_N_nearest(word, sense, n) for sense in range(self.content[word]['sense_number'] + 1)]
+        return res
+
+
 
 class TS_MSW2VModel():
 
